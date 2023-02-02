59729eb3f959596f4fb00c218d9a8f178e8dea7f
Nicolas Fränkel <nicolas@frankel.ch>
2023-02-02 16:46:44 +0100

    Parameterize bucket parameters

--- src/main/kotlin/BucketFactory.kt
+++ src/main/kotlin/BucketFactory.kt
@@ -3,13 +3,12 @@ package ch.frankel.blog
 import io.github.bucket4j.Bandwidth
 import io.github.bucket4j.Bucket
 import io.github.bucket4j.Refill
-import kotlin.time.Duration
-import kotlin.time.toJavaDuration
+import java.time.Duration
 
 class BucketFactory {
 
     fun create(size: Long, number: Long, duration: Duration): Bucket {
-        val limit = Bandwidth.classic(size, Refill.intervally(number, duration.toJavaDuration()))
+        val limit = Bandwidth.classic(size, Refill.intervally(number, duration))
         return Bucket.builder()
             .addLimit(limit)
             .build()
--- src/main/kotlin/ResilientBootApp.kt
+++ src/main/kotlin/ResilientBootApp.kt
@@ -1,19 +1,25 @@
 package ch.frankel.blog
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.context.support.beans
 import org.springframework.web.reactive.function.server.coRouter
-import kotlin.time.Duration.Companion.seconds
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class ResilientBootApplication
 
 fun main(args: Array<String>) {
 
     val beans = beans {
         bean {
-            BucketFactory().create(1, 1, 1.seconds)
+            val props = ref<BucketProperties>()
+            BucketFactory().create(
+                props.size,
+                props.refresh.tokens,
+                props.refresh.duration
+            )
         }
         bean {
             coRouter {
